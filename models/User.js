6d1a7e7d2a1b8a2f57d7d9a44a2e2e0eb4cf0a0f
Tushar Khairnar <tusharkhairnar247@gmail.com>
2026-06-07 17:36:24 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -25,9 +25,6 @@ const User = sequelize.define("User", {
     type: DataTypes.ENUM("ADMIN", "USER", "STORE_OWNER"),
     defaultValue: "USER",
   },
-}, {
-  tableName: "users",      
-  freezeTableName: true,
 });
 
 module.exports = User;
