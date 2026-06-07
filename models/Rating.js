5649dcf82c9501238f64f0b9a74db949599a0db8
Tushar Khairnar <tusharkhairnar247@gmail.com>
2026-06-07 16:40:52 +0530

    Update Rating.js

--- models/Rating.js
+++ models/Rating.js
@@ -40,6 +40,9 @@ Rating.afterUpdate(async (rating) => {
 
 Rating.afterDestroy(async (rating) => {
   await recalcStoreAverage(rating.storeId);
+}{
+  tableName: "ratings",      
+  freezeTableName: true,
 });
 
 module.exports = Rating;
